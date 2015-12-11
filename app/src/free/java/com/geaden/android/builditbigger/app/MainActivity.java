b30f54dd38a56773013bbb0f9d4dbbf344a7c3eb
Gennady Denisov <denisovgena@gmail.com>
2015-12-11 12:07:54 +0300

    Add interstitial Ad for free app version.

---
+++ app/src/free/java/com/geaden/android/builditbigger/app/MainActivity.java
@@ -0,0 +1,78 @@
+package com.geaden.android.builditbigger.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Launches activity with a joke content.
+     *
+     * @param view the view method invoked from.
+     */
+    public void tellJoke(View view) {
+        mInterstitial = new InterstitialAd(this);
+        mInterstitial.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        mInterstitial.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                new JokeTellerAsyncTask().execute(MainActivity.this);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+                new JokeTellerAsyncTask().execute(MainActivity.this);
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                /** Display interstitial ad */
+                mInterstitial.show();
+            }
+        });
+        AdRequest ar = new AdRequest.Builder().build();
+        mInterstitial.loadAd(ar);
+    }
+}
--- app/src/free/java/com/geaden/android/builditbigger/app/MainActivityFragment.java
+++ app/src/free/java/com/geaden/android/builditbigger/app/MainActivityFragment.java
@@ -1,19 +1,22 @@
 package com.geaden.android.builditbigger.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private InterstitialAd mInterstitial;
 
     public MainActivityFragment() {
     }
